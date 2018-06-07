c453137182c04361e88e83f3f63c5a4b0170262d
Jereme <jereme.daniels@gmail.com>
2018-06-07 11:01:51 -0400

    just playing. Changed the text color to a toggle.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,12 @@
 $('h1').click(e => {
-  $(e.target).css('color', 'blue')
+  let color = $(e.target).css('color');
+  if (color == 'rgb(231, 231, 231)'){
+    $(e.target).css('color', 'blue');
+  } else if (color == 'rgb(0, 0, 255)'){
+    $(e.target).css('color', '#e7e7e7');
+  };
 });
+/* $('h1').click(e => {
+  $(e.target).css('color', 'blue');
+
+}); */
